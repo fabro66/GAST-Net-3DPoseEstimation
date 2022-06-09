89ae5a866b8467191e4b4a81293527b6acb6e4b9
yotaroshimose <Yotaro.Shimose@sony.com>
2022-06-09 01:52:41 +0000

    h36m keypointsという概念を導入

--- rhpe/core.py
+++ rhpe/core.py
@@ -3,13 +3,11 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TypedDict
 
 import cv2
 import numpy as np
-from common.camera import camera_to_world, normalize_screen_coordinates
+from common.camera import camera_to_world
 from common.skeleton import Skeleton
-from tools.preprocess import h36m_coco_format
 
 
 class KeyPointDetector(ABC):
@@ -66,47 +64,6 @@ class KeyPoints2D:
     height: int
     meta: KeyPointsMeta
     valid_frames: np.ndarray
-
-    @classmethod
-    def from_coco(
-        cls, coordinates: np.ndarray, scores: np.ndarray, width: int, height: int
-    ) -> KeyPoints2D:
-        """constructor from coco-formatted numpy
-
-        Args:
-            coordinate (np.ndarray): (F, J, 2)
-            scores (np.ndarray): (F, )
-
-        Returns:
-            KeyPoints2D: _description_
-        """
-        joints_left = [4, 5, 6, 11, 12, 13]
-        joints_right = [1, 2, 3, 14, 15, 16]
-        coordinates, scores, valid_frames = h36m_coco_format(
-            coordinates[np.newaxis], scores[np.newaxis]
-        )
-        skeleton = Skeleton(
-            parents=[-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15],
-            joints_left=[4, 5, 6, 11, 12, 13],
-            joints_right=[1, 2, 3, 14, 15, 16],
-        )
-        meta = KeyPointsMeta(
-            skeleton=skeleton,
-            keypoints_symmetry=(joints_left, joints_right),
-            layout_name="Human3.6M",
-            num_joints=17,
-        )
-        return cls(
-            coordinates=coordinates[0],
-            scores=scores[0],
-            width=width,
-            height=height,
-            meta=meta,
-            valid_frames=valid_frames[0],
-        )
-
-    def as_input(self) -> np.ndarray:
-        return normalize_screen_coordinates(self.coordinates, self.width, self.height)
 
 
 @dataclass
